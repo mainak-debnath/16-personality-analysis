ff0ae78c70217b9b27969fcb24976a0b101fb2bc
Mainak Debnath <debnath.mainak07@gmail.com>
2021-09-19 18:18:04 +0530

    added models

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
+from nltk.stem.porter import PorterStemmer
 from flask import Flask, render_template, url_for, request
+import pickle
+import nltk
+from nltk.corpus import stopwords
+import string
+nltk.download('stopwords')
+ps = PorterStemmer()
 
 app = Flask(__name__)
+
+EorI_model = pickle.load(open('model_ie.pickle', 'rb'))
+SorN_model = pickle.load(open("model_ns.pickle", 'rb'))
+TorF_model = pickle.load(open("model_tf.pickle", 'rb'))
+JorP_model = pickle.load(open("model_jp.pickle", 'rb'))
+tfidf = pickle.load(open('vectorizer.pickle', 'rb'))
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
 
 
 @app.route('/')
@@ -17,9 +51,33 @@ def base():
 @app.route('/predict', methods=['POST'])
 def predict():
     string = 'Your Personality Type Is '
+    pType = ''
     if request.method == 'POST':
-        value = request.form['message']
-        return render_template('base.html', prediction_text=string + str(value))
+        text = request.form['message']
+        transformed_text = transform_text(text)
+        vector_input = tfidf.transform([transformed_text])
+        result1 = EorI_model.predict(vector_input)[0]
+        result2 = SorN_model.predict(vector_input)[0]
+        result3 = TorF_model.predict(vector_input)[0]
+        result4 = JorP_model.predict(vector_input)[0]
+        if result1 == 0:
+            pType += 'I'
+        else:
+            pType += 'E'
+        if result2 == 0:
+            pType += 'N'
+        else:
+            pType += 'S'
+        if result3 == 0:
+            pType += 'T'
+        else:
+            pType += 'F'
+        if result4 == 0:
+            pType += 'J'
+        else:
+            pType += 'P'
+
+        return render_template('base.html', prediction_text=string + str(pType))
 
 
 if __name__ == "__main__":
